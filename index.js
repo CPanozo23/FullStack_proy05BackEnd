02c651031597f3010542c0138036e30c835994c6
CPanozo23 <cpanozo@outlook.com>
2023-09-02 01:22:09 -0400

    no cors

--- index.js
+++ index.js
@@ -20,11 +20,22 @@ mongoose.connect(process.env.MONGO_URI);
 
 const port = process.env.PORT;
 
+
+
+//app.use(cors(corsOptions))
+app.use(cors())
+
+/* 
+
+const corsOptions={
+    ['http://localhost:5173', 'https://carolinapanozo.netlify.app'],
+    optionsSuccessStatus:200
+}
 const corsOptions={
     origin:[process.env.FRONTEND_URL, process.env.FRONT_URL_DEV],
     optionsSuccessStatus:200
 }
-app.use(cors(corsOptions))
+app.use(cors(corsOptions))*/
 app.use(express.json())
 app.use('/users',userRoutes)
 app.use('/consultations',consultationRoutes)
