54659c30f2e46d7aa21cbc952a885da78579e13c
CPanozo23 <cpanozo@outlook.com>
2023-09-02 00:35:31 -0400

    2 cors optios

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ mongoose.connect(process.env.MONGO_URI);
 const port = process.env.PORT;
 
 const corsOptions={
-    origin:process.env.FRONTEND_URL,
+    origin:[process.env.FRONTEND_URL, process.env.FRONT_URL_DEV],
     optionsSuccessStatus:200
 }
 app.use(cors(corsOptions))
